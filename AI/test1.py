5651e77c5dc07f6fa99faf85eb4814bf16e3afcd
liuxingyvhuo <1973240747@qq.com>
2025-03-15 10:03:55 +0800

    Delete AI directory

--- AI/test1.py
+++
@@ -1,8 +0,0 @@
-import torch
-import whisper
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model = whisper.load_model("turbo").to(device)
-
-result = model.transcribe("udp_uploads/combined_audio.mp3")
-print(result["text"])
